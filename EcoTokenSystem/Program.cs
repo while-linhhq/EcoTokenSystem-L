05e586abe62e9617f1217c847119fc94c00e2886
nguyentrongtot <nguyentrongtot.it@gmail.com>
2025-11-18 00:14:51 +0700

    Add project files.

---
+++ EcoTokenSystem/Program.cs
@@ -0,0 +1,51 @@
+﻿
+namespace EcoTokenSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            // add service CORS in container
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      // port 5173)
+                                      policy.WithOrigins("http://localhost:5173")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod()
+                                            .AllowCredentials(); //   cookie or header JWT
+                                  });
+            });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
